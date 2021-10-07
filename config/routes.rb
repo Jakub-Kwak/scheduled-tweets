35dac29b599bbba793e4eb3aa4a8bf400bfbf17c
Jakub-Kwak <jakub.kwak@toptal.com>
2021-10-07 15:30:45 +0200

    Add sign up, login, change password panels

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get "about-us", to: "about#index", as: :about
+  get "about", to: "about#index", as: :about
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "password/reset", to: "password_reset#new"
+  post "password/reset", to: "password_reset#create"
+
+  get "password/reset", to:
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
 end
