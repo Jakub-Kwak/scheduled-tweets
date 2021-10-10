606ff9ce0495add527b24d02f61f3922b1ec3492
Jakub-Kwak <jakub.kwak@toptal.com>
2021-10-10 18:24:16 +0200

    Add OmniaAuth

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   get "password", to: "passwords#edit", as: :edit_password
   patch "password", to: "passwords#update"
 
-  get "password/reset", to: "password_reset#new"
-  post "password/reset", to: "password_reset#create"
-
-  get "password/reset", to:
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
